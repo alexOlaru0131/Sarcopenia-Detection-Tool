240aef7c849b4018531bd40f43c8237a5346e562
alexOlaru0131 <alexolaru131@gmail.com>
2025-09-10 22:15:51 +0300

    ./: Added new GUI functions
    - Added a new function to generate the first tab
    - Added image controller for placing an image in the tab
    - Added event handler for position click
    
    Signed-off-by: alexOlaru0131 <alexolaru131@gmail.com>

--- imports.py
+++ imports.py
@@ -5,36 +5,43 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
+# from torchvision import transforms
+# from torch.utils.data import Dataset, DataLoader
 
 # Pillow //
+import PIL
 from PIL import Image, ImageTk
 
 # OpenCV //
 import cv2
 
 # SciPy //
-from scipy.ndimage import gaussian_filter, label, center_of_mass, binary_dilation, binary_opening, binary_erosion, find_objects, center_of_mass, binary_dilation, binary_fill_holes
-from scipy.signal import convolve2d
+# from scipy.ndimage import gaussian_filter, label, center_of_mass, binary_dilation, binary_opening, binary_erosion, find_objects, center_of_mass, binary_dilation, binary_fill_holes
+# from scipy.signal import convolve2d
 
 # Sci-kit //
-from skimage.exposure import rescale_intensity
-from skimage.measure import regionprops, label as sk_label
-from skimage.morphology import skeletonize
+# from skimage.exposure import rescale_intensity
+# from skimage.measure import regionprops, label as sk_label
+# from skimage.morphology import skeletonize
 
 # Other imports //
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pydicom
-import dicom2jpg
+# import dicom2jpg
 import shutil
 import tkinter as tk
+import time
+import screeninfo
+from screeninfo import get_monitors
+from tkinter import *
+from tkinter.filedialog import askopenfilename
 import tqdm
 from tkinter import ttk, filedialog, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from functools import partial
 from tqdm import tqdm
+from threading import Thread, Event, Lock
 
 ###### END imports.py ######
